d72fbb47d370b41dadaddb7068d324e0a6b18d31
Reman Child <rchild@okta.com>
2017-04-02 23:28:29 -0700

    Add users to groups
    - Assign users to groups created from stormpath groups
    
    RESOLVES OKTA-118834

--- migrators/migrate-groups.js
+++ migrators/migrate-groups.js
@@ -1,19 +1,35 @@
-const Promise = require('bluebird');
 const createOktaGroup = require('../functions/create-okta-group');
+const addUsersToGroup = require('../functions/add-users-to-group');
 const logger = require('../util/logger');
 const stormpathExport = require('../stormpath/stormpath-export');
+const config = require('../util/config');
+const addUsersFromGroup = require('./util/add-users-from-group');
+const cache = require('./util/cache');
 
-function migrateGroups() {
-  logger.header('Starting groups import');
-  logger.warn('TODO: assign users');
-  const groups = stormpathExport.getGroups();
-  const pending = [];
-  for (let group of groups) {
-    const name = `group:${group.json.directory.id}:${group.json.name}`;
-    const description = group.json.description;
-    pending.push(createOktaGroup(name, description));
+async function migrateGroup(membershipMap, stormpathGroup) {
+  const lg = logger.group(`Stormpath group id=${stormpathGroup.id} name=${stormpathGroup.name}`);
+  try {
+    const name = `group:${stormpathGroup.directory.id}:${stormpathGroup.name}`;
+    const description = stormpathGroup.description;
+    const oktaGroup = await createOktaGroup(name, description);
+    cache.groupMap[stormpathGroup.id] = oktaGroup.id;
+    await addUsersFromGroup(stormpathGroup.id);
+  } catch (err) {
+    logger.error(err);
+  } finally {
+    lg.end();
   }
-  return Promise.all(pending);
+}
+
+async function migrateGroups() {
+  logger.header('Starting groups import');
+
+  const stormpathGroups = stormpathExport.getGroups();
+  logger.info(`Importing ${stormpathGroups.length} groups`);
+
+  const membershipMap = await stormpathExport.getGroupMembershipMap();
+  const migrate = migrateGroup.bind(null, membershipMap);
+  return stormpathGroups.each(migrate, { limit: 1 });
 }
 
 module.exports = migrateGroups;
